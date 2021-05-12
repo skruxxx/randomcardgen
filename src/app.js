ef1ba758356a1f23808894fb042564846a8b0da4
skruxxx <overlorda@yahoo.com>
2021-05-12 14:21:36 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// <!-- ♦ ♥ ♠ ♣ -->
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let number = document.querySelector(".number");
+  const symbols = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "J",
+    "Q"
+  ];
+  let randomSymbol = symbols[Math.floor(Math.random() * 12)];
+  number.innerHTML = randomSymbol;
+  let cornerSymbols = document.querySelectorAll(".suit");
+  let suits = ["&hearts;", "&diams;", "&spades;", "&clubs;"];
+  let randomSuits = suits[Math.floor(Math.random() * 4)];
+
+  cornerSymbols.forEach(suit => {
+    suit.innerHTML = randomSuits;
+    if (randomSuits == "&hearts;" || randomSuits == "&diams;") {
+      suit.style.color = "red";
+      number.style.color = "red";
+    } else suit.style.color = "black";
+  });
 };
